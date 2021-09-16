54a4748e0e444f52d795cbf933caa14743c14a9a
Ravi Bhushan Singh <54642001+ravi16222@users.noreply.github.com>
2021-09-16 13:15:33 +0530

    Springboot using redis database

---
+++ src/main/java/Exampe3/redis/UserRepository.java
@@ -0,0 +1,12 @@
+package Exampe3.redis;
+
+import java.util.Map;
+
+public interface UserRepository {
+
+    void save(User user);
+    Map<String, User> findAll();
+    User findById(String id);
+    void update(User user);
+    void delete(String id);
+}
